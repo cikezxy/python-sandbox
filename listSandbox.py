0227d3c5e06b72d1532e945bf40d78b57182f21a
James.Zheng <James.Zheng@homecredit.cn>
2020-12-03 09:07:42 +0800

    111

---
+++ listSandbox.py
@@ -0,0 +1,30 @@
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# thislist.append("pear")
+# print(thislist)
+
+# 删除元素
+# thislist = ["apple", "banana", "cherry","pear"]
+# del thislist[2]
+# print(thislist)
+# thislist.remove('banana')
+# print(thislist)
+# thislist.pop()
+# print(thislist)
+# thislist.clear()
+# print(thislist)
+# del thislist
+# print(thislist)
+#
+# alph = ["a", "b", "c", "d"]
+# ralph = reversed(alph)
+# print(list(ralph)) #['d', 'c', 'b', 'a']
+# print(alph) #['a', 'b', 'c', 'd']
+# alph.reverse()
+# print(alph) #['d', 'c', 'b', 'a']
+
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+thislist.append("pear")
+print(mylist)
+print(thislist)
